8bb8f67e57efd34ffb9c22decc865fdf8f8642aa
jibauch94 <jibba_000@10.200.128.51>
2017-03-01 10:58:10 +0100

    InfantryFactory created

---
+++ factory-game/src/OrcInfantryFactory.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jibba_000
+ */
+public class OrcInfantryFactory extends InfantryFactory {
+    public InfantryUnit createUnit(int techLevel) {
+        InfantryUnit character;
+        switch (techLevel) {
+            case 1:
+                character = new RockHauler(3, 10, 10, 5);
+                break;
+            case 2:
+                character = new SpearTower(4, 11, 12, 6);
+                break;
+            case 3:
+                character = new Archer(5, 12, 14, 7);
+                break;
+            case 4:
+                character = new CrossbowMan(6, 13, 16, 8);
+                break;
+            case 5:
+                character = new Musketeer(1, 14, 18, 9);
+
+            default:
+                return null;
+
+        }
+        return character;
+    }
+    
+}
