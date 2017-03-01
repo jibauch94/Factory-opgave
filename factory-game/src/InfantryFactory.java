1ca40008f8f4f580d290b8646aadf7acc8062de3
jibauch94 <jibba_000@10.200.128.51>
2017-03-01 11:00:53 +0100

    InfantryFactory created

---
+++ factory-game/src/InfantryFactory.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jibba_000
+ */
+public abstract class InfantryFactory {
+    public InfantryUnit creatUnit(int techLevel){
+        return null;
+    }
+}
